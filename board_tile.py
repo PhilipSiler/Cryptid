c87f13b6c3e9e21ec722f21ce81b787e745850a9
Philip David Siler <phil.siler@gmail.com>
2023-04-28 23:46:51 -0400

    added randomization and 1d hex array

--- board_tile.py
+++ board_tile.py
@@ -3,35 +3,34 @@ import random
 ###GAME VARIABLES GO HERE
 num_tiles = 108
 num_structures = 8
-
-class Board:
-    def __init__(self) -> None:
-        self.pieces = random.shuffle(self.pieces)
-        self.structure_indices = random.sample(range(0,num_tiles), num_structures)
-
-        #returns a randomly-ordered array of range(1,7) 
-    def random_board_maker(self) -> list[int]:
-        board_nums = [i for i in range(1,7)]
-        random.shuffle(board_nums)
-        return board_nums
-
-    class Tile:
+class Tile:
         def __init__(self, terrain, structure=None, animal=None, cubes=None) -> None:
             self.terrain_type = terrain
             self.structure = structure
             self.animal = animal
             self.cubes = cubes
 
-    #big 18-hex pieces, indexed to zero (the big piece labeled 1 is index 0 here)
-    pieces = [[Tile("WATER"),Tile("WATER"),Tile("WATER"),Tile("WATER"),Tile("FOREST"),Tile("FOREST"),Tile("SWAMP"),Tile("SWAMP"),Tile("WATER"),Tile("DESERT"),Tile("FOREST"),Tile("FOREST"),Tile("SWAMP"),Tile("SWAMP"),Tile("DESERT"),Tile("DESERT"),Tile("DESERT"),Tile("FOREST")],
+class Board:
+    def __init__(self) -> None:
+        self.pieces = self.random_board_maker()#random.shuffle(self.pieces)
+        self.structure_indices = random.sample(range(0,num_tiles), num_structures)
+
+        #returns a randomly-ordered array of range(1,7) 
+    def random_board_maker(self) -> list[list]:
+        pieces = [[Tile("WATER"),Tile("WATER"),Tile("WATER"),Tile("WATER"),Tile("FOREST"),Tile("FOREST"),Tile("SWAMP"),Tile("SWAMP"),Tile("WATER"),Tile("DESERT"),Tile("FOREST"),Tile("FOREST"),Tile("SWAMP"),Tile("SWAMP"),Tile("DESERT"),Tile("DESERT"),Tile("DESERT"),Tile("FOREST")],
               [Tile("SWAMP"),Tile("FOREST"),Tile("FOREST"),Tile("FOREST"),Tile("FOREST"),Tile("FOREST"),Tile("SWAMP"),Tile("SWAMP"),Tile("FOREST"),Tile("DESERT"),Tile("DESERT"),Tile("DESERT"),Tile("SWAMP"),Tile("MOUNTAIN"),Tile("MOUNTAIN"),Tile("MOUNTAIN"),Tile("MOUNTAIN"),Tile("DESERT")],
               [Tile("SWAMP"),Tile("SWAMP"),Tile("FOREST"),Tile("FOREST"),Tile("FOREST"),Tile("WATER"),Tile("SWAMP"),Tile("SWAMP"),Tile("FOREST"),Tile("MOUNTAIN"),Tile("WATER"),Tile("WATER"),Tile("MOUNTAIN"),Tile("MOUNTAIN"),Tile("MOUNTAIN"),Tile("MOUNTAIN"),Tile("WATER"),Tile("WATER"),],
               [Tile("DESERT"),Tile("DESERT"),Tile("MOUNTAIN"),Tile("MOUNTAIN"),Tile("MOUNTAIN"),Tile("MOUNTAIN"),Tile("DESERT"),Tile("DESERT"),Tile("MOUNTAIN"),Tile("MOUNTAIN"),Tile("WATER"),Tile("WATER"),Tile("DESERT"),Tile("DESERT"),Tile("DESERT"),Tile("FOREST"),Tile("FOREST"),Tile("FOREST"),],
               [Tile("SWAMP"),Tile("SWAMP"),Tile("SWAMP"),Tile("MOUNTAIN"),Tile("MOUNTAIN"),Tile("MOUNTAIN"),Tile("DESERT"),Tile("DESERT"),Tile("MOUNTAIN"),Tile("MOUNTAIN"),Tile("WATER"),Tile("WATER"),Tile("DESERT"),Tile("DESERT"),Tile("DESERT"),Tile("FOREST"),Tile("FOREST"),Tile("FOREST"),],
               [Tile("DESERT"),Tile("DESERT"),Tile("SWAMP"),Tile("SWAMP"),Tile("SWAMP"),Tile("FOREST"),Tile("MOUNTAIN"),Tile("MOUNTAIN"),Tile("SWAMP"),Tile("SWAMP"),Tile("FOREST"),Tile("FOREST"),Tile("MOUNTAIN"),Tile("WATER"),Tile("WATER"),Tile("WATER"),Tile("WATER"),Tile("FOREST"),]
              ]
-    
-    random.shuffle(pieces)
+        random.shuffle(pieces)
+        pieces_combined = []
+        for piece in pieces:
+            if random.choice([True, False]):
+                piece = piece[::-1]
+            pieces_combined.extend(piece)
+        return pieces_combined
 
 class Structure:
     def __init__(self, color, type) -> None:
@@ -61,4 +60,5 @@ structures = structure_maker()
 board_arr = [None] * 108
 
 my_board = Board()
-print(vars(my_board))
+for hex in my_board.pieces:
+    print(vars(hex))
